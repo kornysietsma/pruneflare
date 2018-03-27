576eb98a4ed19dcd95c968fc242f436ea2305cb6
Korny Sietsma <korny@sietsma.com>
2018-03-27 12:44:20 +0100

    updated library versions, bumped version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject pruneflare "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject pruneflare "1.0.0"
+  :description "simple tool to prune directories from a d3 flare json file"
   :url "https://github.com/kornysietsma/pruneflare"
   :license {:name "Apache 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [cheshire "5.5.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [cheshire "5.8.0"]]
   :main pruneflare.cli
   :uberjar-name "pruneflare.jar"
   :profiles {
              :uberjar {:aot :all}
-             :dev     {:dependencies [[midje "1.8.3"]]}
+             :dev     {:dependencies [[midje "1.9.1"]]}
              }
   :plugins [[lein-midje "3.2"]]
   )
